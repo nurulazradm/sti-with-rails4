5df0b82b34084a29f0e2b39047f06d2eac582a8b
Nurulazrad Murad <nurulazrad@gmail.com>
2016-02-12 14:36:50 +0800

    Add scopes to parent model for each child model and delegates in tribe model

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -2,6 +2,10 @@ class Animal < ActiveRecord::Base
   belongs_to :tribe
   self.inheritance_column = :race
 
+  scope :lions, -> { where(race: 'Lion') }
+  scope :meerkats, -> { where(race: 'Meerkat') }
+  scope :wild_boars, -> { where(race: 'WildBoar') }
+
   def self.races
     %w(Lion WildBoar Meerkat)
   end
