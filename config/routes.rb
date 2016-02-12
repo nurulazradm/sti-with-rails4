2d90f02ac064078346450c8cca75ffc3af0071e3
Nurulazrad Murad <nurulazrad@gmail.com>
2016-02-12 15:48:44 +0800

    Define routes and the corresponding views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :animals
+  resources :lions, controller: 'animals', type: 'Lion'
+  resources :meerkats, controller: 'animals', type: 'Meerkat'
+  resources :wild_boars, controller: 'animals', type: 'WildBoar'
+
+  root 'animals#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
